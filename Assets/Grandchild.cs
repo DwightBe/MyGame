e915a8425009daad4e4cc74e24c3d88c7888b704
Dwight Benignus <dwightbenignus@gmail.com>
2014-12-26 23:26:39 -0600

    Unity Game Project

---
+++ Assets/Grandchild.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grandchild : MonoBehaviour {
+
+	public float moveSpeed = 2f;		// The speed the enemy moves at.
+	
+	void Awake()
+	{
+
+	}
+
+	void FixedUpdate ()
+	{
+		rigidbody2D.velocity = new Vector2(transform.localScale.x * moveSpeed, rigidbody2D.velocity.y);	
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
