5577b7b40c0b32bb43fb13d48ec88cf6ba5b3378
Dwight Benignus <dwightbenignus@gmail.com>
2015-01-09 15:04:57 -0600

    Music and art
    music and art updates

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+	void Awake () {
+		//Debug.Log (Screen.height);
+		//camera.orthographicSize = Screen.height / 2;
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public bool facingRight = true;			// For determining which way the player is currently facing.
+
+	public float moveForce = 365f;			// Amount of force added to move the player left and right.
+	public float maxSpeed = 1f;				// The fastest the player can travel in the x axis.
+	public float speed = 0.5f;
+	public bool trigger = false;
+	public Transform myTarget;
+
+
+	private Animator anim;						// Reference to the Animator on the player
+
+			
+	void Awake()
+	{
+		// Setting up the references.
+		anim = GetComponent<Animator>();
+	}
+
+	void FixedUpdate ()
+	{	
+		// Cache the horizontal input.
+		float h = Input.GetAxis("Horizontal");
+		//float v = Input.GetAxis("Vertical");
+
+		anim.SetFloat("Speed", Mathf.Abs(h));
+
+		// If the player is changing direction (h has a different sign to velocity.x) or hasn't reached maxSpeed yet...
+		if(h * rigidbody2D.velocity.x < maxSpeed)
+			// ... add a force to the player.
+			rigidbody2D.AddForce(Vector2.right * h * moveForce);
+		// If the player's horizontal velocity is greater than the maxSpeed...
+		if(Mathf.Abs(rigidbody2D.velocity.x) > maxSpeed)
+			// ... set the player's velocity to the maxSpeed in the x axis.
+			rigidbody2D.velocity = new Vector2(Mathf.Sign(rigidbody2D.velocity.x) * maxSpeed, rigidbody2D.velocity.y);
+
+		// If the player's horizontal velocity is greater than the maxSpeed...
+		if(Mathf.Abs(rigidbody2D.velocity.y) > maxSpeed)
+			// ... set the player's velocity to the maxSpeed in the x axis.
+			rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, Mathf.Sign(rigidbody2D.velocity.y) * maxSpeed);
+			
+		// If the input is moving the player right and the player is facing left...
+		if(h > 0 && !facingRight)
+			// ... flip the player.
+			Flip();
+		// Otherwise if the input is moving the player left and the player is facing right...
+		else if(h < 0 && facingRight)
+			// ... flip the player.
+			Flip();
+
+	}
+	
+	
+	void Flip ()
+	{
+		// Switch the way the player is labelled as facing.
+		facingRight = !facingRight;
+		
+		// Multiply the player's x local scale by -1.
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+
+	void OnTriggerStay2D(Collider2D other)
+	{
+		//myTarget = other.gameObject.transform;
+
+		Debug.Log (other.gameObject);
+
+
+
+	}
+	
+	void OnTriggerExit2D(Collider2D other)
+	{
+
+
+
+	}
+}
